c753392214ef4de26602092303c62eac367379af
mariibarra14 <mariangelicaibarraluces@gmail.com>
2025-11-17 00:46:49 -0400

    initial commit
    Commit inicial del MS de Pagos

---
+++ Pagos.API/Controllers/PagosController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pagos.API.Controllers
+{
+    [ApiController]
+    [Route("/api/Pagos")]
+    public class PagosController : ControllerBase
+    {
+
+    }
+}
